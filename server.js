dd03b50235db44224c1fc2050ba12b97feddce20
nicholasjamessmith <njsmith6@gmail.com>
2023-08-09 18:55:12 -0400

    model updated

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express")
 const app = express()
 const { PORT = 8000, DATABASE_URL } = process.env
 
+//Model
+const BookmarkSchema = new mongoose.Schema({
+  name: String,
+  url: String,
+});
+
+const Bookmark = mongoose.model("Bookmark", BookmarkSchema);
+
 app.get("/", (req, res) => {
-    res.json({hello: "world"})
+  res.json({ hello: "world" })
 })
 
 ///////////////////////////////////////////////////
