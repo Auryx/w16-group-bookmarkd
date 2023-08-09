45ea13e1a32dc9438663cf33840a95ba2ae73a97
Auryx <lilbizb1@gmail.com>
2023-08-09 13:30:54 -0700

    added middleware, connect tracker for mongoose, and fin dependencies

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+// DEPENDENCIES
+require("dotenv").config();
+const { PORT = 8000, DATABASE_URL } = process.env
 const express = require("express")
 const app = express()
-const { PORT = 8000, DATABASE_URL } = process.env
+const mongoose = require("mongoose")
+const cors = require("cors")
+const morgan = require("morgan")
 
+// Connection Tracker for Mongoose
 
+mongoose.connect(DATABASE_URL)
 
+mongoose.connection
+.on("open", () => console.log("Mongo connected"))
+.on("close", () => console.log("Mongo disconnected"))
+.on("error", () => console.log(error))
+
+// Middleware
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+/////////////////////////////////////////
 
 // ROUTES
 
