81a1135ddfe87c1e778eed46d6a2b9f99f14d304
Susie Gordon <susieyoungchoi@gmail.com>
2023-08-08 22:26:53 -0400

    added index route

--- server.js
+++ server.js
@@ -2,9 +2,25 @@ const express = require("express")
 const app = express()
 const { PORT = 8000, DATABASE_URL } = process.env
 
+
+
+
+// ROUTES
+
+// index
+app.get('/bookmarks', async(req, res) => {
+    try {
+        const bookmarks = await Bookmarks.find({});
+        res.json(bookmarks);
+    } catch (error) {
+        res.status(400).json({error});
+    }
+});
+
 app.get("/", (req, res) => {
     res.json({hello: "world"})
 })
+
 
 ///////////////////////////////////////////////////
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
